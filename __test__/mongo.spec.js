0cee4c2b432bdbf41be39343151095fb14a1c9f4
Millie Swain <swa19001@byui.edu>
2023-05-27 00:58:39 -0700

    fix text errors

--- __test__/mongo.spec.js
+++ __test__/mongo.spec.js
@@ -14,26 +14,20 @@ describe('insert', () => {
     });
     afterAll(async() => {
         await connection.close() 
-    })
-
+    }),
     it('should insert a new user into the users collection', async () => {
-        const users = db.collection('users');
-
-        const mockUser = {
-            id: 'some-user-id',
-            firstName: "Emily",
-            lastName: "Button",
-            email: "emilyButton@gmail.com",
-            age: 25,
-        }
-
-        await users.insertOne(mockUser)
-
-        const insertedUser = await users.findOne({ id: 'some-user-id' });
-
-        expect(insertedUser).toEqual(mockUser)
+    const users = db.collection('users')
+    const mockUser = {
+        id: 'some-user-id',
+        firstName: "Emily",
+        lastName: "Button",
+        email: "emilyButton@gmail.com",
+        age: 25,
+    }
+    await users.insertOne(mockUser)
+    const insertedUser = await users.findOne({ id: 'some-user-id' });
+    expect(insertedUser).toEqual(mockUser)
     },
-        
     it('should delete a user from the users collection', async () => {
         const users = db.collection('users')
         await users.deleteMany({ id: 'some-user-id' })
