442ff173baa8859ca508cdcfdb99aa42a08dc86c
Millie Swain <swa19001@byui.edu>
2023-05-28 22:51:46 -0700

    fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
 
-/* const users = require('./routes/users') 
+/* const users = require('./routes/users') */
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -17,14 +17,13 @@ app
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .use(cookieParser())
-
   /* .use('/', users) */
-  
   .use(session({
     secret: "secret",
     resave: false,
     saveUninitialized: true,
   }))
+  
   // This is the basic expres session({..}) initialization.
   .use(passport.initialize())
   // init pasport on every route call
@@ -45,14 +44,6 @@ app
   })
   app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
   app.use(cors({ origin: '*'}))
-
-  // added 5/26/23 
-function userReportHandler(req, res) {
-  res.render('userreport', { title: 'Users Report' });
-}
-
-app.get('/users/report', userReportHandler);
-// end 5/26 modification
 
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
@@ -83,6 +74,14 @@ app.get('/github/callback', passport.authenticate('github', {
   });
 
 module.exports = app;
+
+  // added 5/26/23 
+  function userReportHandler(req, res) {
+    res.render('userreport', { title: 'Users Report' });
+  }
+  
+  app.get('/users/report', userReportHandler);
+  // end 5/26 modification
 
 mongodb.initDb((err) => {
   if (err) {
