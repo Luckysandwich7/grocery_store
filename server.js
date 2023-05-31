ad58d37872a99b92689aec65c30b30afc61b75f8
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-30 22:24:58 -0600

    downlaod for google apis

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
