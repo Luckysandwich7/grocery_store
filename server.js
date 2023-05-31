4c85b30579445ced9b528779294c9fa351f8f948
MasterP2 <arikpopeace98@gmail.com>
2023-05-30 23:54:57 -0400

    Created New Branch for Gmail test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongodb = require('./db/connect.js');
 const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const cors = require('cors');
 
 const port = process.env.PORT || 3000;
@@ -27,24 +28,24 @@ app
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
-      );
+    );
     res.setHeader(
       "Access-Control-Allow-Methods",
       "POST, GET, PUT, PATCH, OPTIONS, DELETE"
     );
     next();
   })
-  app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
-  app.use(cors({ origin: '*'}))
+app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+app.use(cors({ origin: '*' }))
 
-  passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-  },
-  function(accessToken, refreshToken, profile, done) {
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+  function (accessToken, refreshToken, profile, done) {
     //User.findorCreate({githubId: profile.id }, function (err, user) {
-      return done(null, profile);
+    return done(null, profile);
     //}));
   }
 ));
@@ -56,14 +57,61 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-app.get('/', (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out") });
 
 app.get('/github/callback', passport.authenticate('github', {
-  failureRedirect: '/api-docs', session: false}),
+  failureRedirect: '/api-docs', session: false
+}),
   (req, res) => {
     req.session.user = req.user;
     res.redirect('/');
   });
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+// NOTE: Apparently, this is not used
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+// passport.deserializeUser((user, done) => {
+//   done(null, user);
+// });
+
+app.get("/logged-status", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged Out"
+  );
+});
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
+  customer.createCustomer
+);
+
+process.on("uncaughtException", (err, origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
+});
+
 
 mongodb.initDb((err) => {
   if (err) {
