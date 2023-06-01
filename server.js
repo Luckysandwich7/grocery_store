db5bcd3e1521cb971b052c6f2e9eacbe1c4fbff8
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-31 20:52:49 -0600

    remove duplicated Path variable

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-var path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect.js');
