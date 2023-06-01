44af92440dbbc4598cbaa8c128b35c08c138dc31
Luckysandwich7 <Ryanalvord@hotmail.com>
2023-05-31 19:29:59 -0600

    removal of duplicate code

--- __test__/employees.t.js
+++ __test__/employees.t.js
@@ -4,24 +4,9 @@ const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
 
-//  Controller Tests
-describe(' Controller', () => {
-  createControllerTests(Controller.getAll);
-  createControllerTests(Controller.getSingle);
-  createControllerTests(Controller.create);
-  createControllerTests(Controller.update);
-  createControllerTests(Controller.delete);
-});
-
-const employeesController = require('../controllers/employees');
-const { createControllerTests, getAllTest} = require('./test');
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
-dotenv.config();
-
 jest.mock('mongodb');
 
-//  Deli Controller Tests
+//  Employees Controller Tests
 describe(' Deli Controller', () => {
   createControllerTests(employeesController.getAll);
   getAllTest(employeesController, employeesController.getAll);
