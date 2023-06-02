0733290d8caa48611a1aaea191f1cbc060e3f367
MasterP2 <arikpopeace98@gmail.com>
2023-06-01 22:31:18 -0400

    fix bug

--- server.js
+++ server.js
@@ -61,19 +61,19 @@ passport.use(new GitHubStrategy({
   }
 )); 
 
-// // -------       Google Auth       ------//
+// -------       Google Auth       ------//
 
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: process.env.GOOGLE_CALLBACK_URL
-// },
-//   function (accessToken, refreshToken, profile, done) {
-//     //User.findorCreate({githubId: profile.id }, function (err, user) {
-//     return done(null, profile);
-//     //}));
-//   }
-// )); 
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+},
+  function (accessToken, refreshToken, profile, done) {
+    //User.findorCreate({githubId: profile.id }, function (err, user) {
+    return done(null, profile);
+    //}));
+  }
+)); 
 
 
 passport.serializeUser((user, done) => {
